bb42ac669ead796dd11113dc935c7f7fe0667a8f
RoySamson-stack <rsam9009@gmail.com>
2021-11-22 23:12:36 +0300

    adding the navbar for the travel blog

---
+++ src/components/main.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+function Main  (){
+    return (
+      <div className="">
+        <nav className="navbar">
+          <img src="" className="navbar-img"/>
+          <p className="navbar-txt">my travel journal.</p>
+        </nav>
+        <div className="main-container">
+          <div className="">
+            
+          </div>
+        </div>
+      </div>
+    );
+  
+}
+
+export default Main;
