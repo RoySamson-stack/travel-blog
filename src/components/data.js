f5facffddd05dd7a93664a07effdeb5764ba1d99
RoySamson-stack <rsam9009@gmail.com>
2021-11-22 23:40:22 +0300

    making the data for the cards

--- src/components/data.js
+++ src/components/data.js
@@ -1,5 +1,40 @@
 
 
-export data = {
-  
-}
+export default [
+  {
+    id: 1,
+    info:{
+      country: "JAPAN",
+      location: "Mount Fiji",
+    },
+    image: "https://unsplash.com/photos/WLxQvbMyfas",
+    // Australia - https://unsplash.com/photos/JmuyB_LibRo
+    // Norway - https://unsplash.com/photos/3PeSjpLVtLg"
+    date: "12 Jan, 2021-24 Jan, 2021",
+    link: "https://www.mountfiji.com/",
+    description:
+    "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12, 380 feet).\"Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourist." 
+  },
+
+  {
+    id: 2,
+    info:{
+      country: "AUSTRALIA",
+      location: "Sydney Opera House",
+    },
+    image:"https://unsplash.com/photos/JmuyB_LibRo",
+    date:"27 May, 2021-8 Jun, 2021",
+    link:"https://www.sydneyoperahouse.com.au/",
+    description: "The Sydney Opera House is a multi-venue performing arts centre \ in Sydney. Located on the banks of the Sydney Harbour, it is often \ regarded as One of the 20th century's most famous and distinctive \ buildings"
+  },
+  {
+    id: 3,
+    info:{
+      country: "NORWAY",
+      location: "Geirangerfjord",
+    },
+    image:"https://unsplash.com/photos/3PeSjpLVtLg",
+    date:"01 Oct, 2021-18 Nov, 2021",
+    description: "The Geiranger Fjord is a large fjord in the Sunmore region of MOre og \ Romsdale cpunty, Norway.It is located entirely in the Stranda \ Municipality"
+  }
+]
